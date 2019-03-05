36a528decad1a390fa566c939a54d22abd44199b
MrNaif2018 <chuff184@gmail.com>
2019-03-05 23:16:28 +0300

    Whitenoise adjustments

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -58,13 +58,13 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 MIDDLEWARE.extend(config("MIDDLEWARE",default="",cast=Csv()))
