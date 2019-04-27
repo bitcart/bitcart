a3c0a5ba0dbb0127b4063293d5e0895a4822bd26
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2019-04-27 23:22:34 +0300

    add test create product for models

--- gui/tests/test_models.py
+++ gui/tests/test_models.py
@@ -1,6 +1,6 @@
 # pylint: disable=no-member
 import pytest
-from gui.tests.test_views import user, wallet
+from gui.tests.test_views import user, wallet, store
 from gui import views, models
 from django.shortcuts import reverse
 from django.contrib.auth import authenticate
@@ -17,3 +17,23 @@ def test_create_wallet(client, user):
         id=secrets.token_urlsafe(44), name="mywallet", user=user)
     assert wallet.name == 'mywallet'
     assert wallet.user == user
+
+
+def test_create_store(client, wallet):
+    store = models.Store.objects.create(
+        id=secrets.token_urlsafe(44),
+        name="test1", wallet=wallet, domain="example.com",
+        template="default", email="test@example.com")
+    assert store.name == 'test1'
+    assert store.wallet == wallet
+    assert store.template == 'default'
+
+
+def test_create_product(client, store):
+    product = models.Product.objects.create(
+        id=secrets.token_urlsafe(44), amount=1.2, quantity=1,
+        title='test_title', bitcoin_address='37eCSgGyN5zeumL2eHaURnC6YNdmLa9TzH',
+        store=store
+    )
+    assert product.title == 'test_title'
+    assert product.bitcoin_address == '37eCSgGyN5zeumL2eHaURnC6YNdmLa9TzH'
