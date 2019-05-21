d6a6a00d0b79912e7cc4a82bf810bed199534cdc
MrNaif2018 <chuff184@gmail.com>
2019-05-21 21:25:56 +0300

    Big cleanup, performance improvements

--- gui/tests/test_views.py
+++ gui/tests/test_views.py
@@ -35,10 +35,32 @@ def store(wallet):
     yield store
 
 
+@pytest.fixture
+def product(store):
+    product = models.Product.objects.create(
+        id=secrets.token_urlsafe(44),
+        title="product",
+        amount=5.5,
+        quantity=3,
+        store=store,
+        description="Nice!")
+    yield product
+
+
 def test_truncate():
     assert views.truncate("abc", 5) == "abc"
     assert views.truncate("aaaaa", 3) == "aaa.."
     assert views.truncate("test1", 1, endchar="test2") == "ttest2"
+
+
+def test_get_product_dict(product):
+    data = views.get_product_dict(product)
+    assert isinstance(data, dict)
+    assert data['amount'] == 5.5
+    assert data['quantity'] == 3
+    assert data['title'] == 'product'
+    assert data['status'] == 'active'
+    assert data['description'] == 'Nice!'
 
 
 def test_main(client, user):
@@ -59,110 +81,6 @@ def test_stores(client, user, wallet):
     client.post(reverse("stores"), json.dumps(
         kwargs), content_type='application/json')
     assert models.Store.objects.filter(**kwargs).exists()
-
-
-def test_edit_store(client):
-    assert True
-
-
-def test_store_settings(client):
-    assert True
-
-
-def test_filter_products(client):
-    assert True
-
-
-def test_products(client):
-    assert True
-
-
-def test_invoice_buy(client):
-    assert True
-
-
-def test_product_info(client):
-    assert True
-
-
-def test_get_product_dict(client):
-    assert True
-
-
-def test_product_export(client):
-    assert True
-
-
-def test_create_store(client):
-    assert True
-
-
-def test_delete_store(client):
-    assert True
-
-
-def test_register(client):
-    assert True
-
-
-def test_login(client):
-    assert True
-
-
-def test_logout(client):
-    assert True
-
-
-def test_account_settings(client):
-    assert True
-
-
-def test_change_password(client):
-    assert True
-
-
-def test_wallets(client):
-    assert True
-
-
-def test_apps(client):
-    assert True
-
-
-def test_wallet_history(client):
-    assert True
-
-
-def test_locales(client):
-    assert True
-
-
-def test_create_product(client, store, wallet):
-    client.login(username='test', password='test')
-    kwargs = {"title": 'test_title',
-              "amount": "0.5",
-              "quantity": "1",
-              "store": store.pk}
-    client.post(reverse("create_product"), kwargs)
-    assert models.Product.objects.filter(**kwargs).exists()
-
-
-def test_invoice_status(client):
-    assert True
-
-
-def test_create_wallet(client, user):
-    client.login(username='test', password='test')
-    kwargs = {"name": "test1",
-              "user": user.pk,
-              "xpub": TEST_XPUB}
-    client.post(reverse("create_wallet"), json.dumps(
-        kwargs), content_type='application/json')
-    assert models.Wallet.objects.filter(**kwargs).exists()
-
-
-def test_api_wallet_info(client):
-    assert True
 
 
 def test_delete_wallet(client, wallet):
