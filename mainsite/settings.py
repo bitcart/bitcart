a32f1be8b26619beb22fbdcaddb6f0c97aa3f6f1
MrNaif2018 <chuff184@gmail.com>
2019-07-09 18:12:47 +0300

    Attempt to allow accesing from Vue.js

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -87,10 +87,12 @@ REST_FRAMEWORK = {
     ],
 }
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    'http://127.0.0.1:3000'
+# CORS
+CORS_ALLOW_METHODS = (
+    'OPTIONS',
 )
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
