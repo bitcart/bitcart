b22525bceae577d737a3d9c0ef928c34512fd948
MrNaif2018 <chuff184@gmail.com>
2019-08-15 22:03:31 +0300

    Add database configs as in django's bitcart

--- api/db.py
+++ api/db.py
@@ -1,5 +1,8 @@
 from gino import Gino
 
+from .settings import DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
+
 db = Gino()
 
-CONNECTION_STR = 'postgresql://postgres:123@@localhost/gino'
+# format from settings
+CONNECTION_STR = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
--- api/settings.py
+++ api/settings.py
@@ -5,11 +5,22 @@ from starlette.config import Config
 
 config = Config("conf/.env")
 
+# bitcart-related
 RPC_USER = config("RPC_USER", default="electrum")
 RPC_PASS = config("RPC_PASS", default="electrumz")
 RPC_URL = config("RPC_URL", default="http://localhost:5000/")
+
+# redis
 REDIS_HOST = config("REDIS_HOST", default="redis://localhost")
 
+# database
+DB_NAME = config("DB_DATABASE", default="bitcart")
+DB_USER = config("DB_USER", default="postgres")
+DB_PASSWORD = config("DB_PASSWORD", default="123@")
+DB_HOST = config("DB_HOST", default="127.0.0.1")
+DB_PORT = config("DB_PORT", default="5432")
+
+# initialize bitcart instance
 with warnings.catch_warnings():  # it is supposed
     warnings.simplefilter("ignore")
     btc = BTC(RPC_URL, rpc_user=RPC_USER, rpc_pass=RPC_PASS)
