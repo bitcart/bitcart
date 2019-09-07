8036f6151d2093a85e6a7474a2f75f3c40191e05
MrNaif2018 <chuff184@gmail.com>
2019-09-07 15:59:44 +0300

    Full daemon refactor, bump electrum

---
+++ daemons/base.py
@@ -0,0 +1,254 @@
+import asyncio
+import functools
+import inspect
+import os
+import traceback
+from base64 import b64decode
+
+from aiohttp import web
+from decouple import AutoConfig
+from electrum import constants
+from electrum.commands import Commands
+from electrum.daemon import Daemon
+from electrum.logging import configure_logging
+from electrum.simple_config import SimpleConfig
+from electrum.storage import WalletStorage
+from electrum.util import standardize_path
+from electrum.wallet import Wallet
+
+
+def rpc(f):
+    f.is_handler = True
+    return f
+
+
+class BaseDaemon:
+    # initialize coin specific things here
+    name: str
+    AVAILABLE_EVENTS: list = []
+    EVENT_MAPPING: dict = {}
+    NETWORK_MAPPING = {
+        "mainnet": constants.set_mainnet,
+        "testnet": constants.set_testnet,
+        "regtest": constants.set_regtest,
+        "simnet": constants.set_simnet,
+    }
+
+    def __init__(self):
+        # load env variables
+        self.env_name = self.name.upper()
+        self.config = AutoConfig(search_path="conf")
+        self.LOGIN = self.config(f"{self.env_name}_LOGIN", default="electrum")
+        self.PASSWORD = self.config(f"{self.env_name}_PASSWORD", default="electrumz")
+        self.NET = self.config(f"{self.env_name}_NETWORK", default="mainnet")
+        self.LIGHTNING = self.config(
+            f"{self.env_name}_LIGHTNING", cast=bool, default=True
+        )
+        self.DEFAULT_CURRENCY = self.config(
+            f"{self.env_name}_FIAT_CURRENCY", default="USD"
+        )
+        self.VERBOSE = self.config(f"{self.env_name}_DEBUG", cast=bool, default=False)
+        self.HOST = self.config(
+            f"{self.env_name}_HOST",
+            default="0.0.0.0" if os.getenv("IN_DOCKER") else "127.0.0.1",
+        )
+        self.PORT = self.config(f"{self.env_name}_PORT", cast=int, default=5000)
+        self.base_methods = {
+            "get_updates": self.get_updates,
+            "subscribe": self.subscribe,
+            "unsubscribe": self.unsubscribe,
+        }
+        self.supported_methods = {
+            func.__name__: func
+            for func in (getattr(self, name) for name in dir(self))
+            if getattr(func, "is_handler", False)
+        }
+        # activate network and configure logging
+        activate_selected_network = self.NETWORK_MAPPING.get(self.NET.lower())
+        if not activate_selected_network:
+            raise ValueError(
+                f"Invalid network passed: {self.NET}. Valid choices are {', '.join(self.NETWORK_MAPPING.keys())}."
+            )
+        activate_selected_network()
+        electrum_config = SimpleConfig()
+        electrum_config.set_key("verbosity", self.VERBOSE)
+        electrum_config.set_key("lightning", self.LIGHTNING)
+        configure_logging(electrum_config)
+        # initialize wallet storages
+        self.wallets = {}
+        self.wallets_config = {}
+        self.wallets_updates = {}
+        # initialize not yet created network
+        self.network = None
+        self.fx = None
+        self.daemon = None
+
+    async def on_startup(self, app):
+        config = SimpleConfig()
+        config.set_key("currency", self.DEFAULT_CURRENCY)
+        config.set_key("use_exchange_rate", True)
+        self.daemon = Daemon(config, listen_jsonrpc=False)
+        self.network = self.daemon.network
+        self.network.register_callback(self.process_events, self.AVAILABLE_EVENTS)
+        # as said in electrum daemon code, this is ugly
+        config.fee_estimates = self.network.config.fee_estimates.copy()
+        config.mempool_fees = self.network.config.mempool_fees.copy()
+        self.fx = self.daemon.fx
+
+    async def load_wallet(self, xpub):
+        if xpub in self.wallets:
+            wallet_data = self.wallets[xpub]
+            return wallet_data["wallet"], wallet_data["cmd"], wallet_data["config"]
+        config = SimpleConfig()
+        # as said in electrum daemon code, this is ugly
+        config.fee_estimates = self.network.config.fee_estimates.copy()
+        config.mempool_fees = self.network.config.mempool_fees.copy()
+        command_runner = Commands(
+            config=config, network=self.network, daemon=self.daemon
+        )
+        if not xpub:
+            return None, command_runner, config
+        # get wallet on disk
+        wallet_dir = os.path.dirname(config.get_wallet_path())
+        wallet_path = os.path.join(wallet_dir, xpub)
+        if not os.path.exists(wallet_path):
+            config.set_key("wallet_path", wallet_path)
+            await command_runner.restore(xpub, wallet_path=config.get_wallet_path())
+        storage = WalletStorage(wallet_path)
+        wallet = Wallet(storage)
+        wallet.start_network(self.network)
+        while not wallet.is_up_to_date():
+            await asyncio.sleep(0.1)
+        self.wallets[xpub] = {"wallet": wallet, "cmd": command_runner, "config": config}
+        self.wallets_config[xpub] = {"events": set()}
+        self.wallets_updates[xpub] = []
+        self.daemon.wallets[standardize_path(wallet_path)] = wallet
+        return wallet, command_runner, config
+
+    def decode_auth(self, authstr):
+        if not authstr:
+            return None, None
+        authstr = authstr.replace("Basic ", "")
+        decoded_str = b64decode(authstr).decode("latin1")
+        user, password = decoded_str.split(":")
+        return user, password
+
+    async def handle_request(self, request):
+        auth = request.headers.get("Authorization")
+        user, password = self.decode_auth(auth)
+        if not (user == self.LOGIN and password == self.PASSWORD):
+            return web.json_response(
+                {
+                    "jsonrpc": "2.0",
+                    "error": {"code": -32600, "message": "Unauthorized"},
+                    "id": None,
+                }
+            )
+        if request.content_type == "application/json":
+            data = await request.json()
+        else:
+            return web.json_response(
+                {
+                    "jsonrpc": "2.0",
+                    "error": {"code": -32600, "message": "Invalid JSON-RPC."},
+                    "id": None,
+                }
+            )
+        method = data.get("method")
+        id = data.get("id", None)
+        xpub = data.get("xpub")
+        params = data.get("params", [])
+        if not method:
+            return web.json_response(
+                {
+                    "jsonrpc": "2.0",
+                    "error": {"code": -32601, "message": "Procedure not found."},
+                    "id": id,
+                }
+            )
+        try:
+            wallet, cmd, config = await self.load_wallet(xpub)
+        except Exception:
+            if not method in self.supported_methods and not method in self.base_methods:
+                return web.json_response(
+                    {
+                        "jsonrpc": "2.0",
+                        "error": {"code": -32601, "message": "Error loading wallet"},
+                        "id": id,
+                    }
+                )
+        custom = False
+        if method in self.base_methods:
+            exec_method = self.base_methods[method]
+            custom = True
+        if method in self.supported_methods:
+            exec_method = self.supported_methods[method]
+            custom = True
+        else:
+            try:
+                exec_method = getattr(cmd, method)
+            except AttributeError:
+                return web.json_response(
+                    {
+                        "jsonrpc": "2.0",
+                        "error": {"code": -32601, "message": "Procedure not found."},
+                        "id": id,
+                    }
+                )
+        try:
+            if custom:
+                exec_method = functools.partial(exec_method, wallet=xpub)
+            else:
+                exec_method = functools.partial(
+                    exec_method, wallet_path=wallet.storage.path if wallet else None
+                )
+            if isinstance(params, list):
+                result = exec_method(*params)
+            elif isinstance(params, dict):
+                result = exec_method(**params)
+            if inspect.isawaitable(result):
+                result = await result
+        except Exception:
+            return web.json_response(
+                {
+                    "jsonrpc": "2.0",
+                    "error": {
+                        "code": -32601,
+                        "message": traceback.format_exc().splitlines()[-1],
+                    },
+                    "id": id,
+                }
+            )
+        return web.json_response(
+            {"jsonrpc": "2.0", "result": result, "error": None, "id": id}
+        )
+
+    async def process_events(self, event, *args):
+        mapped_event = self.EVENT_MAPPING.get(event)
+        data = {"event": mapped_event}
+        try:
+            data_got, wallet = self._process_events(mapped_event, *args)
+        except Exception:
+            return
+        if data_got is None:
+            return
+        data.update(data_got)
+        for i in self.wallets_config:
+            if mapped_event in self.wallets_config[i]["events"]:
+                if not wallet or wallet == self.wallets[i].wallet:
+                    self.wallets_updates[i].append(data)
+
+    def get_updates(self, wallet):
+        updates = self.wallets_updates[wallet]
+        self.wallets_updates[wallet] = []
+        return updates
+
+    def subscribe(self, events, wallet=None):
+        self.wallets_config[wallet]["events"].update(events)
+
+    def unsubscribe(self, events=None, wallet=None):
+        if events is None:
+            events = self.EVENT_MAPPING.keys()
+        self.wallets_config[wallet]["events"] = set(
+            i for i in self.wallets_config[wallet]["events"] if i not in events
+        )
--- daemons/btc.py
+++ daemons/btc.py
@@ -1,273 +1,58 @@
-import asyncio
-import functools
-import inspect
-import os
-import threading
-import time
-import traceback
-from base64 import b64decode, b64encode
-
 from aiohttp import web
-from decouple import AutoConfig
-from electrum import constants
-from electrum.bitcoin import address_to_scripthash
-from electrum.commands import Commands
-from electrum.daemon import Daemon
-from electrum.logging import configure_logging
-from electrum.simple_config import SimpleConfig
-from electrum.storage import WalletStorage
-from electrum.synchronizer import Synchronizer, SynchronizerBase
 from electrum.transaction import Transaction
-from electrum.wallet import Wallet
 
-config = AutoConfig(search_path="conf")
-
-LOGIN = config("BTC_LOGIN", default="electrum")
-PASSWORD = config("BTC_PASSWORD", default="electrumz")
-NET = config("BTC_NETWORK", default="mainnet")
-LIGHTNING = config("BTC_LIGHTNING", cast=bool, default=True)
-DEFAULT_CURRENCY = config("BTC_FIAT_CURRENCY", default="USD")
-
-# events
-AVAILABLE_EVENTS = ["blockchain_updated", "new_transaction"]
-EVENT_MAPPING = {
-    "blockchain_updated": "new_block",
-    "new_transaction": "new_transaction",
-}
+from base import BaseDaemon, rpc
 
 
-def decode_auth(authstr):
-    if not authstr:
-        return None, None
-    authstr = authstr.replace("Basic ", "")
-    decoded_str = b64decode(authstr).decode("latin1")
-    user, password = decoded_str.split(":")
-    return user, password
+class BTCDaemon(BaseDaemon):
+    name = "BTC"
+    AVAILABLE_EVENTS = ["blockchain_updated", "new_transaction"]
+    EVENT_MAPPING = {
+        "blockchain_updated": "new_block",
+        "new_transaction": "new_transaction",
+    }
 
+    def _process_events(self, event, *args):
+        wallet = None
+        data = {}
+        if event == "blockchain_updated":
+            data["height"] = self.network.get_local_height()
+        elif event == "new_transaction":
+            wallet, tx = args
+            data["tx"] = tx.txid()
+        else:
+            return None, None
+        return data, wallet
 
-async def get_transaction(tx, wallet=None):
-    result = await network.interface.session.send_request(
-        "blockchain.transaction.get", [tx, True]
-    )
-    result_formatted = Transaction(result).deserialize()
-    result_formatted.update({"confirmations": result.get("confirmations", 0)})
-    return result_formatted
-
-
-def exchange_rate(currency=DEFAULT_CURRENCY, wallet=None) -> str:
-    if fx.get_currency() != currency:
-        fx.set_currency(currency)
-    return str(fx.exchange_rate())
-
-
-def list_currencies(wallet=None) -> list:
-    return fx.get_currencies(True)
-
-
-def get_tx_size(raw_tx: dict, wallet=None) -> int:
-    return Transaction(raw_tx).estimated_size()
-
-
-def get_updates(wallet):
-    global wallets_updates
-    updates = wallets_updates[wallet]
-    wallets_updates[wallet] = []
-    return updates
-
-
-def subscribe(events, wallet=None):
-    wallets_config[wallet]["events"].update(events)
-
-
-def unsubscribe(events=EVENT_MAPPING.keys(), wallet=None):
-    wallets_config[wallet]["events"] = set(
-        i for i in wallets_config[wallet]["events"] if i not in events
-    )
-
-
-async def process_events(event, *args):
-    data = {"event": EVENT_MAPPING.get(event)}
-    wallet_only = False
-    if event == "blockchain_updated":
-        data["height"] = network.get_local_height()
-    elif event == "new_transaction":
-        wallet, tx = args
-        wallet_only = True
-        data["tx"] = tx.txid()
-    else:
-        return
-    for i in wallets_config:
-        if EVENT_MAPPING.get(event) in wallets_config[i]["events"]:
-            if not wallet_only or wallet == wallets[i].wallet:
-                wallets_updates[i].append(data)
-
-
-wallets = {}
-wallets_updates = {}
-wallets_config = {}
-supported_methods = {
-    "get_transaction": get_transaction,
-    "exchange_rate": exchange_rate,
-    "get_updates": get_updates,
-    "list_currencies": list_currencies,
-    "subscribe": subscribe,
-    "get_tx_size": get_tx_size,
-    "unsubscribe": unsubscribe,
-}
-
-# verbosity
-VERBOSE = config("BTC_DEBUG", cast=bool, default=False)
-NETWORK_MAPPING = {
-    "mainnet": constants.set_mainnet,
-    "testnet": constants.set_testnet,
-    "regtest": constants.set_regtest,
-    "simnet": constants.set_simnet,
-}
-activate_selected_network = NETWORK_MAPPING.get(NET.lower())
-if not activate_selected_network:
-    raise ValueError(
-        f"Invalid network passed: {NET}. Valid choices are mainnet, testnet, regtest and simnet."
-    )
-# activate selected network
-activate_selected_network()
-
-electrum_config = SimpleConfig()
-electrum_config.set_key("verbosity", VERBOSE)
-electrum_config.set_key("lightning", LIGHTNING)
-configure_logging(electrum_config)
-
-
-async def load_wallet(xpub):
-    if xpub in wallets:
-        return wallets[xpub]
-    config = SimpleConfig()
-    # as said in electrum daemon code, this is ugly
-    config.fee_estimates = network.config.fee_estimates.copy()
-    config.mempool_fees = network.config.mempool_fees.copy()
-    command_runner = Commands(config, wallet=None, network=network)
-    if not xpub:
-        return command_runner
-    # get wallet on disk
-    wallet_dir = os.path.dirname(config.get_wallet_path())
-    wallet_path = os.path.join(wallet_dir, xpub)
-    if not os.path.exists(wallet_path):
-        config.set_key("wallet_path", wallet_path)
-        await command_runner.restore(xpub)
-    storage = WalletStorage(wallet_path)
-    wallet = Wallet(storage)
-    wallet.start_network(network)
-    command_runner.wallet = wallet
-    # lightning worker
-    command_runner.lnworker = wallet.lnworker
-    while not wallet.is_up_to_date():
-        await asyncio.sleep(0.1)
-    wallets[xpub] = command_runner
-    wallets_config[xpub] = {"events": set()}
-    wallets_updates[xpub] = []
-    return command_runner
-
-
-async def xpub_func(request):
-    auth = request.headers.get("Authorization")
-    user, password = decode_auth(auth)
-    if not (user == LOGIN and password == PASSWORD):
-        return web.json_response(
-            {
-                "jsonrpc": "2.0",
-                "error": {"code": -32600, "message": "Unauthorized"},
-                "id": None,
-            }
+    @rpc
+    async def get_transaction(self, tx, wallet=None):
+        result = await self.network.interface.session.send_request(
+            "blockchain.transaction.get", [tx, True]
         )
-    if request.content_type == "application/json":
-        data = await request.json()
-    else:
-        return web.json_response(
-            {
-                "jsonrpc": "2.0",
-                "error": {"code": -32600, "message": "Invalid JSON-RPC."},
-                "id": None,
-            }
-        )
-    method = data.get("method")
-    id = data.get("id", None)
-    xpub = data.get("xpub")
-    params = data.get("params", [])
-    if not method:
-        return web.json_response(
-            {
-                "jsonrpc": "2.0",
-                "error": {"code": -32601, "message": "Procedure not found."},
-                "id": id,
-            }
-        )
-    try:
-        wallet = await load_wallet(xpub)
-    except Exception:
-        if not method in supported_methods:
-            return web.json_response(
-                {
-                    "jsonrpc": "2.0",
-                    "error": {"code": -32601, "message": "Error loading wallet"},
-                    "id": id,
-                }
-            )
-    custom = False
-    if method in supported_methods:
-        exec_method = supported_methods[method]
-        custom = True
-    else:
-        try:
-            exec_method = getattr(wallet, method)
-        except AttributeError:
-            return web.json_response(
-                {
-                    "jsonrpc": "2.0",
-                    "error": {"code": -32601, "message": "Procedure not found."},
-                    "id": id,
-                }
-            )
-    try:
-        if custom:
-            exec_method = functools.partial(exec_method, wallet=xpub)
-        if isinstance(params, list):
-            result = exec_method(*params)
-        elif isinstance(params, dict):
-            result = exec_method(**params)
-        if inspect.isawaitable(result):
-            result = await result
-    except Exception:
-        return web.json_response(
-            {
-                "jsonrpc": "2.0",
-                "error": {
-                    "code": -32601,
-                    "message": traceback.format_exc().splitlines()[-1],
-                },
-                "id": id,
-            }
-        )
-    return web.json_response(
-        {"jsonrpc": "2.0", "result": result, "error": None, "id": id}
-    )
+        result_formatted = Transaction(result).deserialize()
+        result_formatted.update({"confirmations": result.get("confirmations", 0)})
+        return result_formatted
+
+    @rpc
+    def exchange_rate(self, currency=None, wallet=None) -> str:
+        if currency is None:
+            currency = self.DEFAULT_CURRENCY
+        if self.fx.get_currency() != currency:
+            self.fx.set_currency(currency)
+        return str(self.fx.exchange_rate())
+
+    @rpc
+    def list_currencies(self, wallet=None) -> list:
+        return self.fx.get_currencies(True)
+
+    @rpc
+    def get_tx_size(self, raw_tx: dict, wallet=None) -> int:
+        return Transaction(raw_tx).estimated_size()
 
 
-async def on_startup(app):
-    global network, fx, loop
-    config = SimpleConfig()
-    config.set_key("currency", DEFAULT_CURRENCY)
-    config.set_key("use_exchange_rate", True)
-    daemon = Daemon(config, listen_jsonrpc=False)
-    network = daemon.network
-    network.register_callback(process_events, AVAILABLE_EVENTS)
-    # as said in electrum daemon code, this is ugly
-    config.fee_estimates = network.config.fee_estimates.copy()
-    config.mempool_fees = network.config.mempool_fees.copy()
-    fx = daemon.fx
-
+daemon = BTCDaemon()
 
 app = web.Application()
-app.router.add_post("/", xpub_func)
-app.on_startup.append(on_startup)
-host = config("BTC_HOST", default="0.0.0.0" if os.getenv("IN_DOCKER") else "127.0.0.1")
-port = config("BTC_PORT", cast=int, default=5000)
-web.run_app(app, host=host, port=port)
+app.router.add_post("/", daemon.handle_request)
+app.on_startup.append(daemon.on_startup)
+web.run_app(app, host=daemon.HOST, port=daemon.PORT)
