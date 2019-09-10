a1a6b1174da63c3941e238c263ac2a1d82f0c48f
MrNaif2018 <chuff184@gmail.com>
2019-09-10 18:57:24 +0300

    Litecoin lightning network support!

--- daemons/ltc.py
+++ daemons/ltc.py
@@ -7,24 +7,12 @@ from base import BaseDaemon, rpc
 class LTCDaemon(BaseDaemon):
     name = "LTC"
     electrum = electrum_ltc
-    NEW_ELECTRUM = False
     AVAILABLE_EVENTS = ["blockchain_updated", "new_transaction"]
     EVENT_MAPPING = {
         "blockchain_updated": "new_block",
         "new_transaction": "new_transaction",
     }
     DEFAULT_PORT = 5001
-
-    def create_commands(self, config):
-        return self.electrum.commands.Commands(
-            config=config, network=self.network, wallet=None
-        )
-
-    async def restore_wallet(self, command_runner, xpub, config):
-        await command_runner.restore(xpub)
-
-    def load_cmd_wallet(self, cmd, wallet, wallet_path):
-        cmd.wallet = wallet
 
     def _process_events(self, event, *args):
         wallet = None
