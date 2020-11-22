dc8db6a400e59d741d1901ac577d27bb8e28f871
MrNaif2018 <chuff184@gmail.com>
2020-11-22 16:06:48 +0300

    Fix worker start on mac os

--- api/logserver.py
+++ api/logserver.py
@@ -1,7 +1,9 @@
 import datetime
 import logging
 import logging.handlers
+import socket
 import socketserver
+import time
 from decimal import Decimal
 
 import msgpack
@@ -54,6 +56,20 @@ class LogRecordSocketReceiver(socketserver.ThreadingTCPServer):
             if rd:
                 self.handle_request()
             abort = self.abort
+
+
+def wait_for_port(host="localhost", port=logging.handlers.DEFAULT_TCP_LOGGING_PORT, timeout=5.0):
+    start_time = time.perf_counter()
+    while True:
+        try:
+            with socket.create_connection((host, port), timeout=timeout):
+                break
+        except OSError as ex:
+            time.sleep(0.01)
+            if time.perf_counter() - start_time >= timeout:
+                raise TimeoutError(
+                    "Waited too long for the port {} on host {} to start accepting " "connections.".format(port, host)
+                ) from ex
 
 
 def main():
--- worker.py
+++ worker.py
@@ -7,15 +7,12 @@ from api import invoices, settings
 from api.ext import tor as tor_ext
 from api.ext import update as update_ext
 from api.logserver import main as start_logserver
+from api.logserver import wait_for_port
 from api.utils import run_repeated
-
-process = Process(target=start_logserver)
-process.start()
 
 
 async def main():
     await settings.init_db()
-    await asyncio.sleep(1)
     settings.log_startup_info()
     await tor_ext.refresh(log=False)  # to pre-load data for initial requests
     await update_ext.refresh()
@@ -30,5 +27,9 @@ def handler(signum, frame):
     sys.exit()
 
 
-signal.signal(signal.SIGINT, handler)
-asyncio.get_event_loop().run_until_complete(main())
+if __name__ == "__main__":
+    process = Process(target=start_logserver)
+    process.start()
+    wait_for_port()
+    signal.signal(signal.SIGINT, handler)
+    asyncio.get_event_loop().run_until_complete(main())
