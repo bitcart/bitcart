186b49514d1ba00c1f89cbb171f46c383662265c
MrNaif2018 <chuff184@gmail.com>
2020-12-21 17:17:23 +0300

    Fix coverage

--- api/crud.py
+++ api/crud.py
@@ -164,7 +164,7 @@ async def _create_payment_method(invoice, wallet, product, store, discounts, pro
 async def create_payment_method(invoice, wallet, product, store, discounts, promocode):
     method = await _create_payment_method(invoice, wallet, product, store, discounts, promocode)
     coin_settings = settings.crypto_settings.get(wallet.currency.lower())
-    if coin_settings and coin_settings["lightning"]:
+    if coin_settings and coin_settings["lightning"]:  # pragma: no cover
         await _create_payment_method(invoice, wallet, product, store, discounts, promocode, lightning=True)
     return method
 
