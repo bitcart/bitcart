fcb2b5890dd5b861d5ed730d3175000ba24ca204
MrNaif2018 <chuff184@gmail.com>
2021-06-01 18:31:57 +0300

    Added IPN logs

--- api/utils/notifications.py
+++ api/utils/notifications.py
@@ -1,17 +1,24 @@
+import traceback
+
 import notifiers
 from aiohttp import ClientSession
 
 from api import models, utils
+from api.logger import get_logger
+
+logger = get_logger(__name__)
 
 
 async def send_ipn(obj, status):  # pragma: no cover
     if obj.notification_url:
         data = {"id": obj.id, "status": status}
+        base_log_message = f"Sending IPN with data {data} to {obj.notification_url}"
         try:
             async with ClientSession() as session:
                 await session.post(obj.notification_url, json=data)
+            logger.info(f"{base_log_message}: success")
         except Exception:
-            pass
+            logger.info(f"{base_log_message}: error\n{traceback.format_exc()}")
 
 
 async def notify(store, text):  # pragma: no cover
