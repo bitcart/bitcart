c6c721677d8f7e46874b2f6c5caf1f70466e56b3
MrNaif2018 <chuff184@gmail.com>
2021-09-15 17:11:15 +0300

    Multiarch builds

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,9 +1,6 @@
 set -e
 cd ../bitcart-docker/compose
 echo "Building $1 image"
-docker build -t bitcartcc/$1:$CIRCLE_TAG -f $2 .
-docker tag bitcartcc/$1:$CIRCLE_TAG bitcartcc/$1:stable
-docker push bitcartcc/$1:$CIRCLE_TAG
-docker push bitcartcc/$1:stable
+docker buildx build --push --platform ${BUILD_PLATFORMS} --tag mrnaif/$1:$CIRCLE_TAG --tag mrnaif/$1:test2 -f $2 .
 cd ../../.circleci
 set +e
