5cbdf84cd208f5ad94e30548c0803415aecab5ff
MrNaif2018 <chuff184@gmail.com>
2021-09-17 17:18:46 +0300

    Try manifest creation

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -2,6 +2,11 @@ set -e
 cd ../bitcart-docker/compose
 echo "Building $1 image"
 : "${ARCH:=amd64}"
-docker buildx build --progress plain --push --platform ${BUILD_PLATFORMS} --tag mrnaif/$1:$CIRCLE_TAG-$ARCH --tag mrnaif/$1:stable-$ARCH -f $2 .
+if [ -z ${MANIFEST+x} ]; then
+    docker buildx build --progress plain --push --platform ${BUILD_PLATFORMS} --tag mrnaif/$1:$CIRCLE_TAG-$ARCH --tag mrnaif/$1:stable-$ARCH -f $2 .
+else
+    docker buildx imagetools create -t mrnaif/$1:$CIRCLE_TAG mrnaif/$1:$CIRCLE_TAG-amd64 mrnaif/$1:$CIRCLE_TAG-arm
+    docker buildx imagetools create -t mrnaif/$1:stable mrnaif/$1:stable-amd64 mrnaif/$1:stable-arm
+fi
 cd ../../.circleci
 set +e
