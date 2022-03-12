515e1b339e1a0dd7e88aefed9b1dfa80ff54dad2
MrNaif2018 <chuff184@gmail.com>
2022-03-13 01:47:37 +0300

    Finalize most commands

--- daemons/eth.py
+++ daemons/eth.py
@@ -10,7 +10,7 @@ from decimal import Decimal
 from base import BaseDaemon
 from eth_account import Account
 from eth_account.messages import encode_defunct
-from eth_keys.datatypes import PublicKey
+from eth_keys.datatypes import PrivateKey, PublicKey
 from hexbytes import HexBytes
 from mnemonic import Mnemonic
 from utils import JsonResponse, get_exception_message, hide_logging_errors, periodic_task, rpc
@@ -26,9 +26,29 @@ NO_HISTORY_MESSAGE = "We don't access transaction history to remain lightweight"
 WRITE_DOWN_SEED_MESSAGE = "Please keep your seed in a safe place; if you lose it, you will not be able to restore your wallet."
 ONE_ADDRESS_MESSAGE = "We only support one address per wallet as it is common in ethereum ecosystem"
 GET_PROOF_MESSAGE = "Geth doesn't support get_proof correctly for now"
+NO_MASTER_KEYS_MESSAGE = "As we use only one address per wallet, address keys are used, but not the xprv/xpub"
 
 CHUNK_SIZE = 30
 # TODO: limit sync post-reboot to (60/12)*60=5*60=300 blocks (max expiry time)
+
+STR_TO_BOOL_MAPPING = {
+    "true": True,
+    "yes": True,
+    "1": True,
+    "false": False,
+    "no": False,
+    "0": False,
+}  # common str -> bool conversions
+
+
+def str_to_bool(s):
+    if isinstance(s, bool):
+        return s
+    s = s.lower()
+
+    if s in STR_TO_BOOL_MAPPING:
+        return STR_TO_BOOL_MAPPING[s]
+    return False
 
 
 @dataclass
@@ -51,34 +71,68 @@ def to_dict(obj):
 
 
 @dataclass
+class KeyStore:
+    key: str
+    address: str = None
+    public_key: str = None
+    private_key: str = None
+    seed: str = None
+    account: Account = None
+
+    def is_watching_only(self):
+        return not self.has_seed() and self.private_key is None
+
+    def __post_init__(self):
+        self.account = None
+        try:
+            self.account = Account.from_mnemonic(self.key)
+            self.seed = self.key
+        except Exception:
+            try:
+                self.account = Account.from_key(self.key)
+            except Exception:
+                try:
+                    self.public_key = PublicKey.from_compressed_bytes(Web3.toBytes(hexstr=self.key))
+                    self.address = self.public_key.to_checksum_address()
+                except Exception:
+                    if not Web3.isAddress(self.key) and not Web3.isChecksumAddress(self.key):
+                        raise Exception("Error loading wallet: invalid address")
+                    self.address = Web3.toChecksumAddress(self.key)
+        if self.account:
+            self.address = self.account.address
+            self.private_key = self.account.key.hex()
+            self.public_key = PublicKey.from_private(PrivateKey(Web3.toBytes(hexstr=self.private_key)))
+        if self.public_key:
+            self.public_key = Web3.toHex(self.public_key.to_compressed_bytes())
+
+    def add_privkey(self, privkey):
+        try:
+            account = Account.from_key(privkey)
+        except Exception:
+            raise Exception("Invalid key provided")
+        if account.address != self.address:
+            raise Exception("Invalid private key imported: address mismatch")
+        self.private_key = privkey
+        self.account = account
+
+    def has_seed(self):
+        return bool(self.seed)
+
+
+@dataclass
 class Wallet:
     key: str
     web3: Web3
     BLOCK_TIME: int = 5
     ADDRESS_CHECK_TIME: int = 60
-    address: str = None
+    keystore: KeyStore = field(init=False)
     path: str = ""
     updates: dict = field(default_factory=dict)
     invoices: dict = field(default_factory=dict)
     pending_invoices: dict = field(default_factory=dict)
 
-    @staticmethod
-    def get_address(key):
-        try:
-            return Account.from_mnemonic(key).address
-        except Exception:
-            try:
-                return Account.from_key(key).address
-            except Exception:
-                try:
-                    return PublicKey.from_compressed_bytes(Web3.toBytes(hexstr=key)).to_checksum_address()
-                except Exception:
-                    if not Web3.isAddress(key) and not Web3.isChecksumAddress(key):
-                        raise Exception("Error loading wallet: invalid address")
-                    return Web3.toChecksumAddress(key)
-
     def __post_init__(self):
-        self.address = self.get_address(self.key)
+        self.keystore = KeyStore(key=self.key)
         self.running = False
         self.loop = asyncio.get_event_loop()
 
@@ -95,6 +149,21 @@ class Wallet:
     def is_synchronized(self):  # because only one address is used due to eth specifics
         return True
 
+    def is_watching_only(self):
+        return self.keystore.is_watching_only()
+
+    def get_private_key(self):
+        if self.is_watching_only():
+            raise Exception("This is a watching-only wallet")
+        return self.keystore.private_key
+
+    def import_private_key(self, privkey):
+        self.keystore.add_privkey(privkey)
+
+    @property
+    def address(self):
+        return self.keystore.address
+
     async def balance(self):
         return await self.web3.eth.get_balance(self.address)
 
@@ -110,6 +179,7 @@ class ETHDaemon(BaseDaemon):
         "clear_invoices": "clear_requests",
         "commands": "help",
         "get_transaction": "gettransaction",
+        "getunusedaddress": "getaddress",
     }
     SKIP_NETWORK = ["getinfo"]
 
@@ -155,12 +225,17 @@ class ETHDaemon(BaseDaemon):
 
     async def process_pending(self):
         while self.running:
-            current_height = await self.web3.eth.block_number
-            tasks = []
-            for block_number in range(self.latest_height + 1, current_height + 1, CHUNK_SIZE):
-                tasks.append(self.process_block(block_number, min(block_number + CHUNK_SIZE - 1, current_height)))
-            await asyncio.gather(*tasks)
-            self.latest_height = current_height
+            try:
+                current_height = await self.web3.eth.block_number
+                tasks = []
+                for block_number in range(self.latest_height + 1, current_height + 1, CHUNK_SIZE):
+                    tasks.append(self.process_block(block_number, min(block_number + CHUNK_SIZE - 1, current_height)))
+                await asyncio.gather(*tasks, return_exceptions=True)
+                self.latest_height = current_height
+            except Exception:
+                if self.VERBOSE:
+                    print("Error processing pending blocks:")
+                    print(traceback.format_exc())
             await asyncio.sleep(self.BLOCK_TIME)
 
     async def on_shutdown(self, app):
@@ -275,12 +350,18 @@ class ETHDaemon(BaseDaemon):
 
     @rpc
     async def broadcast(self, tx, wallet=None):
-        return await self.web3.eth.send_raw_transaction(tx)
+        return to_dict(await self.web3.eth.send_raw_transaction(tx))
 
     @rpc(requires_wallet=True)
     def clear_requests(self, wallet):
         self.wallets[wallet].invoices = {}
         self.wallets[wallet].pending_invoices = {}
+        return True
+
+    @rpc(requires_wallet=True)
+    def close_wallet(self, wallet):
+        self.wallets[wallet].stop()
+        del self.wallets[wallet]
         return True
 
     @rpc
@@ -336,12 +417,37 @@ class ETHDaemon(BaseDaemon):
             "server": self.HTTP_HOST,
             "server_height": numblocks,
             "spv_nodes": len(await self.web3.geth.admin.peers()),
+            "synchronized": not await self.web3.eth.syncing,
             "version": self.VERSION,
         }
+
+    @rpc(requires_wallet=True)
+    def getmasterprivate(self, *args, **kwargs):
+        raise NotImplementedError(NO_MASTER_KEYS_MESSAGE)
 
     @rpc
     def getmerkle(self, *args, **kwargs):
         raise NotImplementedError(GET_PROOF_MESSAGE)
+
+    @rpc(requires_wallet=True)
+    def getmpk(self, *args, **kwargs):
+        raise NotImplementedError(NO_MASTER_KEYS_MESSAGE)
+
+    @rpc(requires_wallet=True)
+    def getprivatekeys(self, *args, wallet=None):
+        if self.wallets[wallet].is_watching_only():
+            raise Exception("This is a watching-only wallet")
+        return self.wallets[wallet].get_private_key()
+
+    @rpc(requires_wallet=True)
+    def getseed(self, *args, wallet=None):
+        if not self.wallets[wallet].keystore.has_seed():
+            raise Exception("This wallet has no seed words")
+        return self.wallets[wallet].keystore.seed
+
+    @rpc(requires_wallet=True)
+    def getpubkeys(self, *args, wallet=None):
+        return self.wallets[wallet].keystore.public_key
 
     @rpc
     def getservers(self, wallet=None):
@@ -362,6 +468,11 @@ class ETHDaemon(BaseDaemon):
         raise NotImplementedError(NO_HISTORY_MESSAGE)
 
     @rpc(requires_wallet=True)
+    def importprivkey(self, privkey, wallet):
+        self.wallets[wallet].import_private_key(privkey)
+        return "Successfully imported"
+
+    @rpc(requires_wallet=True)
     def is_synchronized(self, wallet):
         return self.wallets[wallet].is_synchronized()
 
@@ -379,9 +490,55 @@ class ETHDaemon(BaseDaemon):
             {"path": "", "synchronized": wallet_obj.is_synchronized()} for wallet_obj in self.wallets.values()
         ]  # TODO: add path
 
+    @rpc(requires_wallet=True)
+    async def listaddresses(
+        self, receiving=False, change=False, labels=False, frozen=False, unused=False, funded=False, balance=False, wallet=None
+    ):
+        unused, funded, balance = str_to_bool(unused), str_to_bool(funded), str_to_bool(balance)
+        # NOTE: we mimic electrum APIs, so we keep those unused filters
+        address = self.wallets[wallet].address
+        addr_balance = await self.web3.eth.get_balance(address)
+        ntxs = await self.web3.eth.get_transaction_count(address)
+        if (unused and (addr_balance > 0 or ntxs > 0)) or (funded and addr_balance == 0):
+            return []
+        if balance:
+            return [(address, addr_balance)]
+        else:
+            return [address]
+
     @rpc
     def make_seed(self, nbits=128, language="english", wallet=None):
         return Mnemonic(language).generate(nbits)
+
+    async def get_fee_data(self):
+        block = await self.web3.eth.get_block("latest")
+        max_priority_fee = await self.web3.eth.max_priority_fee
+        max_fee = block.baseFeePerGas * 2 + max_priority_fee
+        return {"maxFeePerGas": max_fee, "maxPriorityFeePerGas": max_priority_fee}
+
+    @rpc(requires_wallet=True)
+    async def payto(self, destination, amount, fee=None, feerate=None, gas=None, unsigned=False, *args, wallet=None):
+        address = self.wallets[wallet].address
+        nonce = await self.web3.eth.get_transaction_count(address)
+        tx_dict = {
+            "type": "0x2",
+            "from": self.wallets[wallet].address,
+            "to": destination,
+            "nonce": nonce,
+            "value": Web3.toWei(amount, "ether"),
+            "chainId": await self.web3.eth.chain_id,
+            "gas": gas or 21000,
+            **(await self.get_fee_data()),
+        }
+        if fee:
+            tx_dict["maxFeePerGas"] = Web3.toWei(fee, "ether")
+        if feerate:
+            tx_dict["maxPriorityFeePerGas"] = Web3.toWei(feerate, "gwei")
+        if unsigned:
+            return tx_dict
+        if self.wallets[wallet].is_watching_only():
+            raise Exception("This is a watching-only wallet")
+        return self._sign_transaction(tx_dict, self.wallets[wallet].keystore.private_key)
 
     @rpc(requires_wallet=True)
     def removelocaltx(self, *args, **kwargs):
@@ -406,6 +563,25 @@ class ETHDaemon(BaseDaemon):
         if not message:
             message = address
         return to_dict(Account.sign_message(encode_defunct(text=message), private_key=self.wallets[wallet].key).signature)
+
+    def _sign_transaction(self, tx, private_key):
+        tx_dict = tx
+        if isinstance(tx, str):
+            try:
+                tx_dict = json.loads(tx)
+            except json.JSONDecodeError as e:
+                raise Exception("Invalid transaction") from e
+        return to_dict(Account.sign_transaction(tx_dict, private_key=private_key).rawTransaction)
+
+    @rpc(requires_wallet=True)
+    def signtransaction(self, tx, wallet=None):
+        if self.wallets[wallet].is_watching_only():
+            raise Exception("This is a watching-only wallet")
+        return self._sign_transaction(tx, self.wallets[wallet].keystore.private_key)
+
+    @rpc
+    def signtransaction_with_privkey(self, tx, privkey, wallet=None):
+        return self._sign_transaction(tx, privkey)
 
     @rpc
     def validateaddress(self, address, wallet=None):
