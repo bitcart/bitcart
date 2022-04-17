d25bf76f5d39ddafc4b16809f4d0f68c6bf25f35
MrNaif2018 <chuff184@gmail.com>
2022-04-17 23:39:03 +0300

    Added sep20 tokens list

--- scripts/regentokens.py
+++ scripts/regentokens.py
@@ -8,8 +8,12 @@ from bs4 import BeautifulSoup
 NAMES = {
     "erc20": {"main_filters": {"slug": "ethereum"}},
     "bep20": {"main_filters": {}, "contract_filters": {"contractPlatform": "BNB Smart Chain (BEP20)"}},
+    "sep20": {},
 }
 API_URL = "https://coinmarketcap.com/tokens/views/all"
+SMARTBCH_URL = "https://www.marketcap.cash"
+SMARTBCH_TOKEN_METADATA = "https://raw.githubusercontent.com/MarketCap-Cash/SmartBCH-Token-List/main/tokens.json"
+SMARTBCH_NUMBER_TOKENS = 50  # fetch top 50 tokens by market cap
 
 
 def exit_err(message):
@@ -44,6 +48,16 @@ def fetch_popular_tokens(filters):
     }
 
 
+def fetch_top50_smartbch():
+    page = requests.get(SMARTBCH_URL)
+    data = get_next_data(page)
+    tokens_meta = requests.get(SMARTBCH_TOKEN_METADATA).json()
+    initial_tokens = data["props"]["pageProps"]["coins"]
+    tokens = sorted(initial_tokens.items(), key=lambda x: x[1]["market_cap"], reverse=True)
+    tokens = tokens[1 : SMARTBCH_NUMBER_TOKENS + 1]  # exclude BCH itself
+    return {token[0]: tokens_meta[token[0]]["address"] for token in tokens}
+
+
 if len(sys.argv) != 2:
     exit_err("Usage: regentokens.py <platform>")
 
@@ -52,7 +66,11 @@ if platform not in NAMES:
     exit_err(f"Unsupported platform: {platform}. Supported ones are: {' '.join(NAMES.keys())}")
 
 filters = NAMES[platform]
-token_symbols = fetch_popular_tokens(filters)
+
+if platform == "sep20":
+    token_symbols = fetch_top50_smartbch()
+else:
+    token_symbols = fetch_popular_tokens(filters)
 
 for token in token_symbols.copy():
     if not token_symbols[token]:
