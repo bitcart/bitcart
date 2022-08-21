ab2e704280c72f6ad6ec4e5df6f458b27b2c1722
MrNaif2018 <chuff184@gmail.com>
2022-08-21 17:41:01 +0300

    Graceful shutdown in worker

--- worker.py
+++ worker.py
@@ -40,20 +40,23 @@ def check_db():
 
 async def main():
     settings = settings_module.settings_ctx.get()
-    await settings_module.init()
-    settings_module.log_startup_info()
-    await tor_ext.refresh(log=False)  # to pre-load data for initial requests
-    await update_ext.refresh()
-    await configurator_ext.refresh_pending_deployments()
-    await backup_ext.manager.start()
-    asyncio.ensure_future(run_repeated(tor_ext.refresh, 60 * 10, 10))
-    asyncio.ensure_future(run_repeated(update_ext.refresh, 60 * 60 * 24))
-    settings.manager.add_event_handler("new_payment", invoices.new_payment_handler)
-    settings.manager.add_event_handler("new_block", invoices.new_block_handler)
-    await invoices.create_expired_tasks()  # to ensure invoices get expired actually
-    coro = events.start_listening(tasks.event_handler)  # to avoid deleted task errors
-    asyncio.ensure_future(coro)
-    await settings.manager.start_websocket(reconnect_callback=invoices.check_pending, force_connect=True)
+    try:
+        await settings_module.init()
+        settings_module.log_startup_info()
+        await tor_ext.refresh(log=False)  # to pre-load data for initial requests
+        await update_ext.refresh()
+        await configurator_ext.refresh_pending_deployments()
+        await backup_ext.manager.start()
+        asyncio.ensure_future(run_repeated(tor_ext.refresh, 60 * 10, 10))
+        asyncio.ensure_future(run_repeated(update_ext.refresh, 60 * 60 * 24))
+        settings.manager.add_event_handler("new_payment", invoices.new_payment_handler)
+        settings.manager.add_event_handler("new_block", invoices.new_block_handler)
+        await invoices.create_expired_tasks()  # to ensure invoices get expired actually
+        coro = events.start_listening(tasks.event_handler)  # to avoid deleted task errors
+        asyncio.ensure_future(coro)
+        await settings.manager.start_websocket(reconnect_callback=invoices.check_pending, force_connect=True)
+    finally:
+        await settings.shutdown()
 
 
 def handler(signum, frame):
