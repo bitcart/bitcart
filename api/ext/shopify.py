a44ba895e6d5dfb6b14212ea0cddd354825557c0
MrNaif2018 <chuff184@gmail.com>
2022-12-11 00:12:00 +0300

    Fix linting

--- api/ext/shopify.py
+++ api/ext/shopify.py
@@ -41,8 +41,10 @@ class ShopifyClient:
         return (
             await self.request(
                 "GET",
-                f"orders/{order_id}.json?fields=id,total_price,total_outstanding,currency"
-                ",presentment_currency,transactions,financial_status",
+                (
+                    f"orders/{order_id}.json?fields=id,total_price,total_outstanding,currency"
+                    ",presentment_currency,transactions,financial_status"
+                ),
             )
         ).get("order", {})
 
