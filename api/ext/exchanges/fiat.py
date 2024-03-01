123d257a2cf8b518c8bd5d73c82c7f37767439b7
Fawaz Ahmed <fawazahmed0@hotmail.com>
2024-03-01 18:35:40 +0530

    fix currency-api link

--- api/ext/exchanges/fiat.py
+++ api/ext/exchanges/fiat.py
@@ -5,6 +5,6 @@ from api.ext.exchanges.base import BaseExchange
 class FiatExchange(BaseExchange):
     async def refresh(self):
         result = await utils.common.send_request(
-            "GET", "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json"
+            "GET", "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json"
         )
         self.quotes = {f"USD_{k.upper()}": utils.common.precise_decimal(v) for k, v in result["usd"].items()}
