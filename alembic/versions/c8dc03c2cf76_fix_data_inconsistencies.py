9d8c76225ba07eff86e60c5907285406b7224bba
MrNaif2018 <chuff184@gmail.com>
2024-08-06 02:33:40 +0300

    More fixes

--- alembic/versions/c8dc03c2cf76_fix_data_inconsistencies.py
+++ alembic/versions/c8dc03c2cf76_fix_data_inconsistencies.py
@@ -19,6 +19,8 @@ def upgrade():
     op.execute("UPDATE products SET templates='{}' where templates IS NULL")
     op.execute("UPDATE products SET image='' where image IS NULL")
     op.execute("UPDATE invoices SET promocode='' where promocode IS NULL")
+    op.execute("UPDATE invoices SET shipping_address='' where shipping_address IS NULL")
+    op.execute("UPDATE invoices SET notes='' where notes IS NULL")
     op.execute("UPDATE wallets SET hint='' where hint IS NULL")
 
 
