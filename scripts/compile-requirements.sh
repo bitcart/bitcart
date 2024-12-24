d9bd774c57f79d56575b0ef4ae0ff6522e159d45
MrNaif2018 <chuff184@gmail.com>
2024-12-25 01:12:43 +0300

    Drop old requirements.txt format

--- scripts/compile-requirements.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-
-for file in requirements/*.txt requirements/daemons/*.txt; do
-    $SCRIPT_DIR/compile-requirement.sh "$file" $@
-done
