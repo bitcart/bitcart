911c13f6275c59750ce8843aaebd15a67ee53682
MrNaif2018 <chuff184@gmail.com>
2025-02-17 23:41:04 +0300

    Allow to change number of api workers

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,6 @@
 import multiprocessing
+import os
 
 bind = "0.0.0.0:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = os.environ.get("BITCART_API_WORKERS", multiprocessing.cpu_count() * 2 + 1)
 worker_class = "uvicorn.workers.UvicornWorker"
