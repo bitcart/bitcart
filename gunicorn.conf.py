4acd75630d3b59970bb88b8a606eb5748e53eeda
MrNaif2018 <chuff184@gmail.com>
2025-02-17 23:48:58 +0300

    Fixes for gunicorn conf

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,5 +2,5 @@ import multiprocessing
 import os
 
 bind = "0.0.0.0:8000"
-workers = os.environ.get("BITCART_API_WORKERS", multiprocessing.cpu_count() * 2 + 1)
+workers = os.environ.get("BITCART_API_WORKERS") or multiprocessing.cpu_count() * 2 + 1
 worker_class = "uvicorn.workers.UvicornWorker"
